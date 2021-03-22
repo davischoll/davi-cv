2868ed2d273bf83d0ac6b3776c337115c816d748
Davi Scholl <davis@bandavide.com.br>
2021-03-21 22:20:09 -0300

    Filtrando a lista de repos e extraindo dados

--- pages/index.js
+++ pages/index.js
@@ -7,7 +7,7 @@ const Index = (props) => {
       <div>{props.currentDate}</div>
       {props.repos.map(repo => {
         return (
-          <div>
+          <div key={repo.id}>
             <h3>{repo.full_name}</h3>
             <p>Language: {repo.language}</p>
             <p>Descrição: {repo.description}</p>
@@ -20,7 +20,29 @@ const Index = (props) => {
 
 export async function getServerSideProps(context) {
   const resRepos = await fetch('https://api.github.com/users/davischoll/repos?sort=updated')
-  const repos = await resRepos.json()
+  const originalReposList = await resRepos.json()
+
+  // MODELO PARA REMOVER ALGUM REPOSITÓRIO:
+
+  const dontShowReposList = ['davischoll/mini-car-phone-holder', 'davischoll/db-access']
+
+  //  VAI FILTRAR OS REPÓSITÓRIOS QUE NÃO SÃO FORK: "fork": true,
+  const isNotFork = repo => !repo.fork
+  //  VAI FILTRAR OS REPÓSITÓRIOS QUE ESTÃO NA LISTA DO VETOR:
+  const dontShowRepos = repo => dontShowReposList.indexOf(repo.full_name) === -1
+  
+  const extractData = repo => ({
+    id: repo.id,
+    full_name: repo.full_name,
+    language: repo.language,
+    description: repo.description
+  })
+
+  const repos = originalReposList
+                  .filter(isNotFork)
+                  .filter(dontShowRepos)
+                  .map(extractData)
+
   return {
     props: {
       currentDate: new Date().toString(),
